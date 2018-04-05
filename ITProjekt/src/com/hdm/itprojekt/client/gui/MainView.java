f2e316c319945ae12f626a420b03ee58015e874e
sinakoritko <sinakoritko@sinas-macbook-pro.fritz.box>
2018-04-05 14:56:27 +0200

    anfangsaenderungen

---
+++ ITProjekt/src/com/hdm/itprojekt/client/gui/MainView.java
@@ -0,0 +1,7 @@
+package com.hdm.itprojekt.client.gui;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class MainView extends Composite {
+
+}
---
+++ ITProjekt/src/com/hdm/itprojekt/shared/bo/Notebook.java
@@ -0,0 +1,5 @@
+package com.hdm.itprojekt.shared.bo;
+
+public class Notebook {
+
+}
