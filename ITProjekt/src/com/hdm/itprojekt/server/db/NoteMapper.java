c89e406c6e150c9fec5faa7631f2f7b6c3ca732c
sinakoritko <sinakoritko@sinas-mbp.fritz.box>
2018-05-11 15:21:43 +0200

    User und Note Mapper Klassen erstellt mit ersten Methoden

--- ITProjekt/src/com/hdm/itprojekt/server/db/NoteMapper.java
+++ ITProjekt/src/com/hdm/itprojekt/server/db/NoteMapper.java
@@ -1,5 +1,156 @@
 package com.hdm.itprojekt.server.db;
 
-public class NoteMapper {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 
+import com.hdm.itprojekt.shared.bo.Note;
+
+/**Notebook Mapper Klasse bildet Note-Objekte auf eine relationale Datenbank ab.
+ * Diese Klasse stellt Methoden zur Verfuegung, die das erstellen, editieren, auslesen/suchen und loeschen 
+ * der gewuenschten Datensaezte erlauben. Die Mapperklasse stellt die Verbindungsschicht zwischen Datenbank
+ * und Applikationslogik dar. Datenbank-Strukturen koennen in Objekte umgewandelt werden, jedoch auch Objekte 
+ * in Datenbankstrukturen
+*/
+
+public class NoteMapper {
+	
+	/**Klasse wird nur einmal instantiiert(Singleton)
+	 * 
+	 */
+	
+	private static NoteMapper noteMapper = null;
+	
+	/**
+	  * Geschützter Konstruktor - verhindert die Möglichkeit, mit new neue
+	  * Instanzen dieser Klasse zu erzeugen.
+	  * 
+	  */
+	
+	protected NoteMapper() {
+	}
+	
+	 /**Diese Methode ist statisch. Sie stellt die 
+	 * Singleton-Eigenschaft sicher, es kann nur eine Instanz von 
+	 * NoteMapper  existieren.
+	 * 
+	 * @return NoteMapper-Objekt
+	 */
+	
+	public static NoteMapper noteMapper(){
+		if (noteMapper == null){
+			noteMapper = new NoteMapper();
+		}
+		
+		return noteMapper;
+	}
+	
+	
+	/**
+	 * Ein Note-Objekt wird in der Datenbank hinzugefuegt. Der Primaerschluessel wird ueberprueft 
+	 * und gegebenenfalls neu zugeordnet.
+	 * @param note (Objekt, das gespeichert werden soll)
+	 * @return neu uebergebenes Objekt, mit angepasster ID
+	 */
+	
+	public Note createNote(Note note){
+		//DB Verbindung erstellen
+		Connection con = DBConnection.connection();
+		
+		try{
+			//Leeres SQL Statement anlegen
+			Statement stmt = con.createStatement();
+			
+			//Statement ausfuellen und als Query an DB senden
+			ResultSet rs = stmt.executeQuery("SELECT MAX(noteID) AS maxnoteID" + "FROM notes");
+			
+			String noteModDate = null;
+			if (note.getNoteModDate() != null){
+				SimpleDateFormat mySQLformat = new SimpleDateFormat("yyyy-MM-dd");
+				noteModDate = mySQLformat.format(note.getNoteModDate());
+			}
+			
+			String noteCreDate = null;
+			if (note.getNoteCreDate() != null){
+				SimpleDateFormat mySQLformat = new SimpleDateFormat("yyyy-MM-dd");
+				noteCreDate = mySQLformat.format(note.getNoteCreDate());
+			}
+			
+			/**
+			 * Es kann max. ein Ergebnis zurueck gegeben werden, da die id der Primaerschluessel ist.
+			 * Daher wird geprueft ob ein Ergebnis vorliegt
+			 */
+			
+			if (rs.next()){
+				//Ergebnis-Tupe in Objekt umwandeln
+				note.setNoteID(rs.getInt("maxnoteID") + 1);
+				stmt = con.createStatement();
+				
+				stmt.executeUpdate("INSERT INTO notes (noteID, noteTitle, noteContent, noteCreDate, noteModDate, userID)"
+									+ "VALUES (" + note.getNoteID() + "," + note.getNoteTitle() + "," 
+									+ note.getNoteContent() + "," + noteCreDate + "," + noteModDate + ")");
+			}
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+		
+		/**
+		 * Note wird zurueckgegeben, da sich das Objekt eventuell im Laufe der Methode veraendert hat
+		 */
+		    
+		return note;
+	}
+	
+	
+	/**
+	 * Note-Objekt wird ueberarbeitet ind ie Datenbank geschrieben
+	 * 
+	 * @param note (Objekt, dass ueberarbeitet in DB geschrieben wird)
+	 * @return als Parameter uebergebenes Objekt.
+	 */
+	
+	public Note updateNote(Note note){
+		Connection con = DBConnection.connection();
+		
+		String noteModDate = null;
+		if (note.getNoteModDate() != null){
+			SimpleDateFormat mySQLformat = new SimpleDateFormat("yyyy-MM-dd");
+			noteModDate = mySQLformat.format(note.getNoteModDate());
+		}
+		
+		try{
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate("UPDATE notes " + "SET title =" + note.getNoteTitle() + "," + "noteContent =" + note.getNoteContent() + "," + "noteModDate =" + noteModDate + "WHERE noteID =" + note.getNoteID());
+		}
+		
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		return note;
+	}
+	
+	
+	
+	/**
+	 * Ein Note-Objekt soll mit seinen Daten aus der DB geloescht werden.
+	 * 
+	 * @param note (Objekt wird aus DB geloescht)
+	 */
+	
+	public void deleteNote(Note note){
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate("DELETE FROM notes" + "WHERE noteID =" + note.getNoteID());
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
 }
--- ITProjekt/src/com/hdm/itprojekt/server/db/UserMapper.java
+++ ITProjekt/src/com/hdm/itprojekt/server/db/UserMapper.java
@@ -1,5 +1,188 @@
 package com.hdm.itprojekt.server.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.hdm.itprojekt.shared.bo.User;
+
 public class UserMapper {
+	
+	//Instanziieren des UserMappers
+	private static UserMapper userMapper = null;
+	
+	/**
+	 * Mithilfe des <code>protected</code>-Attributs im Konstruktor wird
+	 * verhindert, dass von anderen Klassen eine neue Instanz der Klasse
+	 * geschaffen werden kann.
+	 */
+	
+	protected UserMapper(){
+		
+	}
+	
+	/**
+	 * Aufruf eines USER-Mappers fuer Klassen, die keinen Zugriff auf den
+	 * Konstruktor haben.
+	 * 
+	 * @return Einzigartige Mapper-Instanz zur Benutzung in der
+	 *         Applikationsschicht
+	 */
+	
+	public static UserMapper userMapper(){
+		if(userMapper == null){
+			userMapper = new UserMapper();
+		}
+		
+		return userMapper;
+	}
+	
+
+	/**
+	 * Read-Methode - Anhand einer vorgegebenen id wird der dazu gehoerige User
+	 * in der Datenbank gesucht.
+	 * 
+	 * @param userID
+	 *            Die id des Users, der aus der Datenbank gelesen werden soll
+	 * @return Das durch die id referenzierte User-Objekt
+	 */
+
+	public User findByKey(int userID){
+		
+		//DB Verbindung herstellen
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			
+			//Statement ausfuellen und als Query an DB senden
+			ResultSet rs = stmt.executeQuery("SELECT userID, mail, firstname, lastname FROM users" 
+					+ "WHERE userID =" + userID);
+			
+			/**
+			 * Da id Primaerschluessel ist, kann max. nur ein Tupel zurueckgegeben
+			 * werden. Pruefe, ob ein Ergebnis vorliegt.
+			 */
+			
+			if(rs.next()){
+				User user = new User();
+				user.setUserID(rs.getInt("userID"));
+				user.setMail(rs.getString("Mail"));
+				user.setFirstname(rs.getString("firstname"));
+				user.setLastname(rs.getString("lastname"));
+				return user;
+			}
+		} catch (SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+		
+		return null;
+	}
+	
+	
+	/**
+	 * Insert-Methode - Ein User-Objekt wird �bergeben und die zugehoerigen
+	 * Werte in ein SQL-Statement geschrieben, welches ausgef�hrt wird, um das
+	 * Objekt in die Datenbank einzutragen.
+	 * 
+	 * @param user
+	 *            User, der in die Datenbank geschrieben werden soll
+	 * @return User-Objekt, das in die Datenbank geschrieben wurde
+	 */
+	
+	public User insert(User user){
+		
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			
+			/**
+			 * Zunaechst schauen wir nach, welches der momentan hoechste
+			 * Primaerschluesselwert ist.
+			 */
+			
+			ResultSet rs = stmt.executeQuery("SELECT MAX(useriD) AS maxUserID" + "FROM users");
+			
+			if (rs.next()){
+				
+				/**
+				 * user erhaelt den bisher maximalen, nun um 1 inkrementierten
+				 * Primaerschluessel.
+				 */
+				user.setUserID(rs.getInt("maxUserID") + 1);
+				
+				//Neues SQL  Statement erzeugen
+				stmt = con.createStatement();
+				
+				//Einfuegeoperation fuer Statement
+				stmt.executeUpdate("INSERT INTO users (userID, mail, firstname, lastname)" + "VALUES (" + user.getUserID() + "," + user.getMail() + "," + user.getFirstname() + "," + user.getLastname() + ")" );
+				
+				return user;
+				
+			} 
+			
+		}catch (SQLException e){
+			e.printStackTrace();
+				
+			return null;
+		
+		}
+		
+		return null;
+		
+	}
+	
+	/**
+	 * Delete-Methode - Ein User-Objekt wird �bergeben, anhand dessen der
+	 * zugehoerige Eintrag in der Datenbank geloescht wird
+	 * 
+	 * @param user
+	 * User, der geloescht werden soll
+	 */
+
+	public void deleteUser(User user){
+		
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate("DELETE FROM users" + "WHERE userID =" + user.getUserID());
+			
+		} catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	/**
+	 * Edit-Methode - Ein Profil wird uebergeben und die zugehoerigen Werte in ein
+	 * SQL-Statement geschrieben, welches ausgefuehrt wird, um die
+	 * Informationswerte des Profils in der Datenbank zu aktualisieren.
+	 * 
+	 * @param user
+	 *            Der User, dessen Variablen in der DB geaendert werden soll.
+	 */
+
+	public User editUser(User user){
+		
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate("UPDATE users" + "SET firstname =" + user.getFirstname() + "," + "lastname =" + user.getLastname() + "WHERE userID =" + user.getUserID());
+		
+		} catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		return user;
+	}
+	
 
 }
