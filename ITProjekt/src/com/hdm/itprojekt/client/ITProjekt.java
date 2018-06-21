2769487c4a9c20436dc5f321075af3efa7e9ec05
sinakoritko <sinakoritko@141.62.82.200>
2018-06-21 22:07:38 +0200

    Vorbereitungen Implementierung Note Cell List& Erstellen weiterer Callbacks

--- ITProjekt/src/com/hdm/itprojekt/client/ITProjekt.java
+++ ITProjekt/src/com/hdm/itprojekt/client/ITProjekt.java
@@ -1,5 +1,6 @@
 package com.hdm.itprojekt.client;
 
+import com.hdm.itprojekt.shared.bo.Note;
 //import com.hdm.itprojekt.client.ClientsideSettings;
 //import com.hdm.itprojekt.client.gui.LoginService;
 //import com.hdm.itprojekt.client.gui.LoginServiceAsync;
@@ -8,8 +9,14 @@ package com.hdm.itprojekt.client;
 import com.hdm.itprojekt.shared.bo.User;
 import com.hdm.itprojekt.client.gui.Update;
 import com.hdm.itprojekt.shared.FieldVerifier;
+import com.hdm.itprojekt.shared.NoteAdministrationAsync;
 import com.hdm.itprojekt.shared.bo.User;
 import com.hdm.itprojekt.client.gui.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -17,6 +24,11 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
+import com.google.gwt.user.client.Cookies;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Window.Location;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
@@ -28,6 +40,10 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.ProvidesKey;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SingleSelectionModel;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -44,25 +60,56 @@ public class ITProjekt implements EntryPoint {
 	LoginServiceAsync loginService = GWT.create(LoginService.class);
 	private LoginInfo loginInfo = null;
 	
+	private static NoteAdministrationAsync administrationService = ClientsideSettings.getAdministration();
+	
+	private User currentUser = null;
+	
+	TextCell noteCell = new TextCell();
+	
+	private ArrayList<Note> notes = null;
+	
+	NoteCell nCell = new NoteCell();
+	static ProvidesKey<Note> keyProvider = new ProvidesKey<Note>(){
+		public Object getKey(Note item){
+			//Always do a null check
+			return (item == null) ? null : item.getNoteID();
+		}
+	};
+	
+	public final static SingleSelectionModel<String> noteSelectionModel = new SingleSelectionModel<String>();
+	public final static SingleSelectionModel<Note> nSelectionModel = new SingleSelectionModel<Note>(keyProvider);
+	
+	//DataProvider erstellen
+	
+	//public static ListDataProvider<String> noteDataProvider = new ListDataProvider<String>();
+	//public static List <String> noteList = noteDataProvider.getList();
+	
+	public static ListDataProvider<Note> nDataProvider = new ListDataProvider<Note>();
+	public static List<Note> nList = nDataProvider.getList();
+	
+	
 	
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private HorizontalPanel header = new HorizontalPanel();
 	private VerticalPanel contentPanel = new VerticalPanel();
 	private VerticalPanel navigationPanel = new VerticalPanel();
+	//private VerticalPanel navPanel = new VerticalPanel();
+	private VerticalPanel navXtraPanel = new VerticalPanel();
 	private HorizontalPanel footer = new HorizontalPanel();
+	
 	private Label logo = new Label("Notebee");
-	private Label displayUsername = new Label ("username anzeigen");
+	private Label usernameLabel = new Label ();
 	//private Button editUserBtn = new Button("edit User");
 	private Anchor loginLink = new Anchor("login");
 	private Label copyright = new Label("(c) Hochschule der Medien, Sina Koritko");
-	
-	
-	
+	//final CellList<String> noteCellList = new CellList<String>(noteCell);
+	final CellList<Note> nCellList = new CellList<Note>(nCell, keyProvider);
+	final Label noteLabel = new Label();
 	
 	final static Button createNoteBtn = new Button("new note");
 	
 	
-	private User currentUser = null;
+	
 	
 	
 	/**
@@ -92,9 +139,35 @@ public class ITProjekt implements EntryPoint {
 			}
 			
 			public void onSuccess(LoginInfo result) {
+				if(Location.getParameter("url") != null) Cookies.setCookie("url", Location.getParameter("url"));
 				loginInfo = result;
+				ClientsideSettings.setLoginInfo(result);
 				if(loginInfo.isLoggedIn()) {
-					//loadAnfangsbildschirm()
+					administrationService.getUserByMail(loginInfo.getMail(), new AsyncCallback<User>(){
+
+						@Override
+						public void onFailure(Throwable caught) {
+							Window.alert("Fehler: Nutzer konnte in der Datenbank nicht gefunden werden. Details: "+caught);	
+						}
+
+						@Override
+						public void onSuccess(User currentUser) {
+							if(currentUser != null){
+								Cookies.setCookie("userID", String.valueOf(currentUser.getUserID()));
+								loadView();
+								if(Cookies.getCookie("url") != null){
+									Update update = new NoteOverview();
+									RootPanel.get("contentBox").clear();
+									RootPanel.get("contentBox").add(update);	
+								}
+							}else{
+								Cookies.removeCookie("userID");
+								loginInfo.setLoggedIn(false);
+							}
+							
+						}
+						
+					});
 				}
 				else {
 					loadLogin();
@@ -104,9 +177,28 @@ public class ITProjekt implements EntryPoint {
 		
 		});
 		
+	//Auslesen aktuelles Benutzerprofil aus Datenbank
+		
+		administrationService.getUserByMail(ClientsideSettings.getLoginInfo().getMail(), getCurrentUserCallback());
+		
+		/**noteCellList.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);*/
 	
+		//Add selection model to handle user selection
+		
+		/**noteCellList.setSelectionModel(noteSelectionModel); */
+
+		//Connect list to data provider
+		
+		/**noteDataProvider.addDataDisplay(noteCellList);*/
+		
+		nCellList.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+
+		nCellList.setSelectionModel(nSelectionModel);
 	
-	
+		//Conenct list to data provider
+		nDataProvider.addDataDisplay(nCellList);
+		
+		
 		/**Add style names
 		 */
 		
@@ -133,24 +225,32 @@ public class ITProjekt implements EntryPoint {
 		createNoteBtn.addStyleName("createNoteBtn");
 		loginLink.setStyleName("loginLink");
 		logo.setStyleName("logo");
-		displayUsername.setStyleName("displayUsername");
+		usernameLabel.setStyleName("displayUsername");
 		header.setStyleName("header");
 		footer.setStyleName("footer");
 		copyright.setStyleName("copyright");
 		createNoteBtn.setStyleName("btns");
 		
 		
+		
 		/**Zuteilung der Widgets zum jeweiligen Panel
 		 * */
 	
 		
-		contentPanel.add(createNoteBtn);
+		
 
 		
 		header.add(logo);
-		header.add(displayUsername);
+		header.add(usernameLabel);
 		//header.add(editUserBtn);
 		header.add(loginLink);
+		
+		navigationPanel.add(nCellList);
+		//navPanel.add(noteCellList);
+		navigationPanel.add(noteLabel);
+		noteLabel.setText("Waehle eine Notiz aus:");
+		navXtraPanel.add(createNoteBtn);
+	
 		
 		footer.add(copyright);
 		
@@ -164,8 +264,34 @@ public class ITProjekt implements EntryPoint {
 		RootPanel.get("header").add(header);
 		RootPanel.get("contentBox").add(contentPanel);			
 		RootPanel.get("navigation").add(navigationPanel);
+		//RootPanel.get("navigation").add(navPanel);
+		RootPanel.get("navigation").add(navXtraPanel);
 		RootPanel.get("footer").add(footer);
 		
+		
+		  /**
+	     * Implementierung der jeweiligen ClickHandler fuer die einzelnen Widgets
+	     */
+		
+		/**
+		noteSelectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler(){
+			public void onSelectionChange(SelectionChangeEvent event){
+				Update update = new EditNoteView();
+				RootPanel.get("contentBox").clear();
+				RootPanel.get("contentBox").add(update);
+			}
+		});
+		*/
+		
+		nSelectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler(){
+			public void onSelectionChange(SelectionChangeEvent event) {
+				
+				Update update = new EditNoteView();
+				RootPanel.get("contentBox").clear();
+				RootPanel.get("contentBox").add(update);
+				
+			}
+		});
 		
 		
 		
@@ -189,7 +315,61 @@ public class ITProjekt implements EntryPoint {
 	
 	
 	private void loadLogin(){
+		Cookies.setCookie("mail", null);
+		Cookies.setCookie("userID", null);
 		loginLink.setHref(loginInfo.getLoginUrl());
+		
+		Update update = new NoteOverview();
+		RootPanel.get("contentBox").clear();
+		RootPanel.get("contentBox").add(update);
+	}
+	
+	
+	private AsyncCallback<User> getCurrentUserCallback(){
+		AsyncCallback<User> asyncCallback = new AsyncCallback<User>(){
+
+			@Override
+			public void onFailure(Throwable caught) {
+				ClientsideSettings.getLogger().severe("Error: " + caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(User result) {
+				ClientsideSettings.getLogger().severe("Success GetCurrentUserCallback: " + result.getClass().getSimpleName());
+				currentUser = result;
+				
+				usernameLabel.setText(currentUser.getMail());
+				Update update = new NoteOverview();
+				RootPanel.get("contentBox").add(update);
+				
+				administrationService.getNotesOfUser(currentUser, getNotesOfUserCallback());		
+			}
+		};
+		return asyncCallback;
+	}
+	
+	
+	
+	private AsyncCallback<ArrayList<Note>> getNotesOfUserCallback(){
+		AsyncCallback<ArrayList<Note>> asyncCallback = new AsyncCallback<ArrayList<Note>>(){
+
+			@Override
+			public void onFailure(Throwable caught) {
+				ClientsideSettings.getLogger().severe("Error: " + caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(ArrayList<Note> result) {
+				ClientsideSettings.getLogger().severe("Success GetNotesOfUserCallback: " + result.getClass().getSimpleName());
+				notes = result;
+				nDataProvider.getList().clear();
+				for(int i = 0; i < notes.size(); i++){
+					nDataProvider.getList().add(notes.get(i));
+				}
+			}
+			
+		};
+		return asyncCallback;
 	}
 	
 }
