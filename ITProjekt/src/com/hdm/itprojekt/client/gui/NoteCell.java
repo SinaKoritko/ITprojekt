f3f85d5ff08241a286f65b2251e87110935cdcf3
sinakoritko <sinakoritko@141.62.82.200>
2018-06-21 18:57:22 +0200

    Cell Klasse fuer die Darstellung der Navigation von Notiz Objekten erstellt.

---
+++ ITProjekt/src/com/hdm/itprojekt/client/gui/NoteCell.java
@@ -0,0 +1,31 @@
+package com.hdm.itprojekt.client.gui;
+
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.hdm.itprojekt.shared.bo.Note;
+
+/**
+ * Klasse zur Darstellung von Notiz-Objekten.
+ * Solche Erweiterungen von <code>AbstractCell<T></code> dienen zur Erzeugung von
+ * HTML-Code für benutzerdefinierte Objekte. In diesem Fall wird der <code>noteTitle</code>
+ * eines Notizobjektes mit einem vorangestellten "Notiztitel " in einem <code>div-</code>Element
+ * erzeugt.
+ * 
+ */
+
+public class NoteCell extends AbstractCell<Note> {
+
+	@Override
+	public void render(Context context, Note value, SafeHtmlBuilder sb) {
+		//Value can be null, so do a null check
+		
+		if(value == null){
+			return;
+		}
+		
+		sb.appendHtmlConstant("<div notiztitel>");
+		sb.appendEscaped(value.getNoteTitle());
+		sb.appendHtmlConstant("</div>");
+	}
+
+}
